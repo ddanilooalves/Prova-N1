dbc8634053faf098b073eb01a4d907de1fc12a37
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2023-10-03 18:01:56 -0300

    implement exe2 (Prova N1)

---
+++ Avaliação/Ex2.java
@@ -0,0 +1,22 @@
+package Avaliação;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe os lados do seu polígono: ");
+        int n = sc.nextInt();
+
+        int nd = n * (n - 3) / 2;
+
+        if (n < 3) {
+            System.out.println("Um polígono precisa ter mais de 3 lados!");
+            System.out.println("Inicie o programa novamente...");
+        } else {
+            System.out.println("\n### O polígono convexo de " + n + " lados tem " + nd + " diagonais diferentes. ###\n");
+        }
+    }
+    
+}
